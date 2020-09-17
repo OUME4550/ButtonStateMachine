ae75533078904209bdc6f5f9af71c58f13407130
jaywilhelm <jwilhelm@ohio.edu>
2020-09-17 16:56:59 -0400

    Updated time duration check

--- BasicButtonStateMachine.ino
+++ BasicButtonStateMachine.ino
@@ -5,7 +5,7 @@
  */
 
 
-#define ledPin      13
+#define ledPin      7
 #define buttonPin   2
 #define ON_DURATION 1500
 #define DEBUG_MODE //comment out to remove debug statement
@@ -50,11 +50,11 @@ void loop() {
   #endif
   //EVENT LOGIC HERE
   //do something with the state data
-  static unsigned long press_time = millis();
+  static unsigned long press_time = 0;
   if (button1State == 2) { //pressed
     press_time = millis();
-    Serial.println("LED On");
-    digitalWrite(ledPin, HIGH);
+    Serial.print("Start Timer: ");
+    Serial.println(press_time);
     pressCount++;
     Serial.print("Press count: ");
     Serial.println(pressCount);
@@ -66,9 +66,10 @@ void loop() {
     Serial.println("Button Released ");
   }
   //ONLY turn off the LED if it is already on AND time has elapsed
-  else if(digitalRead(ledPin) == HIGH && press_time + ON_DURATION < millis()) {
-    digitalWrite(ledPin, LOW);
-    Serial.println("LED Off - timeout");
+  else if(press_time != 0 && press_time + ON_DURATION < millis()) {
+    Serial.print("Timeout!");
+    Serial.println(millis());
+    press_time = 0;
   }
   #ifdef DEBUG_MODE
   delay(200);
